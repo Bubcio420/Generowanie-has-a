7d6b91a70a96faf2fbf74c2ff3e73ba4f9365528
Bubcio420 <97967665+Bubcio420@users.noreply.github.com>
2022-04-21 15:49:11 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,17 +1,51 @@
 var iloscZn=8;
 var iloscCyfr=0;
+var iloscMaleL=0;
+var iloscDuzeL=0;
+var iloscWolnychZn=0;
+var iloscSpec=0;
 
 function iloscZnakow(){
     iloscZn=document.getElementById("iloscZnakow").value;
-    document.getElementById("iloscCyfr").max=iloscZn;
-    document.getElementById("iloscMaleL").max=iloscZn;
+    
+    ustawWolne();
 }
 
 function wybCyfr(){
-    iloscCyfr=document.getElementById("iloscCyfr").value
+    iloscCyfr=document.getElementById("iloscCyfr").value;
     document.getElementById("wybCyfr").value=iloscCyfr;
+    ustawWolne();
 }
 function wybMaleL(){
-    iloscCyfr=document.getElementById("iloscMaleL").value
-    document.getElementById("wybMaleL").value=iloscCyfr;
+    iloscMaleL=document.getElementById("iloscMaleL").value;
+    document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne();
+}
+function wybDuzeL(){
+    iloscDuzeL=document.getElementById("iloscDuzeL").value;
+    document.getElementById("wybDuzeL").value=iloscDuzeL;
+    ustawWolne();
+}
+function wybSpec(){
+    iloscSpec=document.getElementById("iloscSpec").value;
+    document.getElementById("wybSpec").value=iloscSpec;
+    ustawWolne();
+}
+function ustawWolne(){
+    iloscWolnychZn=+iloscZn-iloscCyfr-iloscMaleL-iloscDuzeL-iloscSpec;
+    document.getElementById("wynik").value=iloscWolnychZn;
+    document.getElementById("iloscCyfr").max=+iloscCyfr+ +iloscWolnychZn;
+    document.getElementById("iloscMaleL").max=+iloscMaleL+ +iloscWolnychZn;
+    document.getElementById("iloscDuzeL").max=+iloscDuzeL+ +iloscWolnychZn;
+    document.getElementById("iloscSpec").max=+iloscSpec+ +iloscWolnychZn;
+}
+function generujHaslo(){
+    let haslo='';
+    const alfabetMale="acbdefghijklmnoprstuvwxyz"
+
+    //losowanie małych liter
+    for(let i=0;i<iloscMaleL;i++){
+        haslo+=alfabetMale.charAt(Math.floor(Math.random()*alfabetMale.length));
+    }
+    document.getElementById("test").value=haslo;
 }
