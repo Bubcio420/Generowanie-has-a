abcf4673d895b0b0fd47a56d199c44a56d3e2447
Bubcio420 <97967665+Bubcio420@users.noreply.github.com>
2022-04-22 08:47:22 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -41,11 +41,34 @@ function ustawWolne(){
 }
 function generujHaslo(){
     let haslo='';
+    const cyfry="0123456789"
     const alfabetMale="acbdefghijklmnoprstuvwxyz"
+    const alfabetDuze="ABCDEFGHIJKLMNOPRSTUVWXYZ"
+    const znakiSpec="-+!@#$%^&*"
 
     //losowanie małych liter
+    for(let i=0;i<iloscCyfr;i++){
+        haslo+=cyfry.charAt(Math.floor(Math.random()*cyfry.length));
+    }
     for(let i=0;i<iloscMaleL;i++){
         haslo+=alfabetMale.charAt(Math.floor(Math.random()*alfabetMale.length));
     }
+    for(let i=0;i<iloscDuzeL;i++){
+        haslo+=alfabetDuze.charAt(Math.floor(Math.random()*alfabetDuze.length));
+    }
+    for(let i=0;i<iloscSpec;i++){
+        haslo+=znakiSpec.charAt(Math.floor(Math.random()*znakiSpec.length));
+    }
     document.getElementById("test").value=haslo;
+}
+function zmianaKolejnosci(haslo){
+    let wynik=haslo.split('');
+    for(let i=0;i<wynik.length;i++){
+        let los=Math.floor(Math.random()*wynik.length);
+        let tmp=wynik[i];
+        wynik[i]=wynik[los];
+        wynik[los]=tmp;
+    }
+    haslo=wynik.join('');
+    return haslo;
 }
